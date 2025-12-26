8520d451cac5e4c5f74c5158702b2f925a85fe37
TheSpyGod <gabrielkorczyk196813@outlook.com>
2025-12-26 12:27:35 +0100

    Fist add

---
+++ engine.hpp
@@ -0,0 +1,12 @@
+#include "curses.h"
+#include "Cursor.hpp"
+#include "interface.hpp"
+#include <algorithm>
+#include <tuple>
+
+class Engine {
+private:
+public:
+    
+    // Map array - Has a 2d version of the game map
+};
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "Loop.cpp"
+
+int main() {
+  
+    Loop l;
+    l.startLoop();
+    return 0;
+}
